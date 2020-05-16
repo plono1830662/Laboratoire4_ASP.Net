b63d293cb6a9e6030fac1334422615fa30234a4f
plono1830662 <plono1830662@etu.cegepjoquiere.ca>
2020-05-16 16:21:24 -0400

    Ajoutez des fichiers projet.

---
+++ Laboratoire4Prog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Laboratoire4Prog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
